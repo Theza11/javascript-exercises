834a28e1255f947ad2ae6fc10d3ba97476f8877a
Theza11 <177869271+Theza11@users.noreply.github.com>
2025-07-13 16:38:38 -0500

    Complete assignment

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let fullString = '';
+    if (number < 0){
+        fullString = 'ERROR';
+        return fullString;
+    }
+    for (i=0; i<number; i++){
+        fullString += string;
+    }
+    return fullString;
 };
 
 // Do not edit below this line
