5254b295f23f290ac651895be0bf7acdd2341df7
Theza11 <177869271+Theza11@users.noreply.github.com>
2025-07-13 17:05:35 -0500

    Complete

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,11 +1,12 @@
 const reverseString = function(phrase) {
-    let letters = [];
+    /*let letters = [];
     let reversedLetters = [];
     letters = phrase.split("");
     for (i=letters.length; i>-1; i--) {
         reversedLetters[letters.length-i] = letters[i];
     };
-    return reversedLetters.join("");
+    return reversedLetters.join("");*/
+    return phrase.split("").reverse().join("");
 };
 
 // Do not edit below this line
